2da42363419787971ee58d965448270e2b8c9143
NewGte <victormartinsoares.25@gmail.com>
2025-05-12 23:15:09 +0000

    Ola, esse e o primeiro passo do desafio de xadrez fiz todos os passos e adicionei algumas coisas espero que goste.

--- xadrez.c
+++ xadrez.c
@@ -1,22 +1,58 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+   //Fiz um menu interativo, para proveitar e treinar outras coisas que aprendir e para dar uma melhorada e mais atrativo.
+int jogador1, i2 = 0, numero;
+char rainha, bispo, torre;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+ printf("******Bem-vindo ao jogo de xadrez******\n");
+ printf("Escolha qual peça que deseja mover:\n");
+ printf("1 para TORRE.\n");
+ printf("2 para BISPO.\n");
+ printf("3 para RAINHA\n");
+ scanf("%d", &jogador1 );
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+ switch (jogador1)
+ {
+ case 1:
+     printf("Torre\n");
+         for ( int i = 0; i < 5; i++){
+             printf("Direita\n");}
+break;  
+ case 2:
+ printf("Bispo\n");
+       while (i2 <= 5)
+    {
+        if(i2 != 0)
+        {
+            printf("direita, cima!\n");
+        }
+        
+        i2++;
+    }
+     break;
+ case 3://Aqui fiz a rainha andar 8 casas, dando a opção para o jogador escolher qual número. sendo que 8 e o verdadeiro.
+    do {
+        printf("Digite o numero 8 para a rainha andar 8 casas.\n");
+        scanf("%d", &numero);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+        if(numero == 8){
+            printf("Rainha: 8 casas para a esquerda\n");
+        } else {
+            printf("Saindo do jogo...\n");
+        }
+        
+    } while(numero < 8 && numero > 8);
+     
+     printf("Obrigado por jogar!\n");
 
+ break;
+ default:
+     printf("Saindo do jogo...\n");
+     break;
+ }
+ 
+ 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
